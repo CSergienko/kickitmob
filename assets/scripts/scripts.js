3710fb6d6f37a566444dd738df26c3f4d7a99fbb
CSergienko <chris@praekelt.com>
2015-05-25 20:27:52 +0200

    Added owl-carousel and mixitup. Made basic grid layout.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
 
     $('[role="header"]').stick_in_parent({
         parent: 'body',
-        sticky_class: 'sticky',
+        sticky_class: 'sticky'
+    });
 
+    $('.event-grid .grid').mixItUp({
+        selectors: {
+            target: '.tile'
+        }
     });
 
 });
