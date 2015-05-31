438e89b0af26fa618e95511f0d29664811178427
CSergienko <chris@praekelt.com>
2015-05-31 12:16:08 +0200

    Refactored events grid page.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,138 +2,180 @@ $(document).ready(function() {
 
     $('[role="header"]').stick_in_parent({
         parent: 'body',
-        sticky_class: 'sticky'
+        sticky_class: 'sticky',
+        selectAll: true,
     });
+ 
+    // To keep our code clean and modular, all custom functionality will be contained inside a single object literal called "buttonFilter".
 
-    
-
-    // To keep our code clean and modular, all custom functionality will be contained inside a single object literal called "dropdownFilter".
-
-    var dropdownFilter = {
-
-    // Declare any variables we will need as properties of the object
-
-    $filters: null,
-    $reset: null,
-    groups: [],
-    outputArray: [],
-    outputString: '',
-
-    // The "init" method will run on document ready and cache any jQuery objects we will need.
-
-    init: function(){
-        var self = this; // As a best practice, in each method we will asign "this" to the variable "self" so that it remains scope-agnostic. We will use it to refer to the parent "dropdownFilter" object so that we can share methods and properties between all parts of the object.
-
-        self.$filters = $('#filters');
-        self.$reset = $('#Reset');
-        self.$container = $('.event-grid .grid');
-
-        self.$filters.find('fieldset:not(.search)').each(function(){
-            self.groups.push({
-                $dropdown: $(this).find('select'),
-                active: ''
-            });
-        });
-
-        self.bindHandlers();
-    },
-
-    // The "bindHandlers" method will listen for whenever a select is changed. 
-
-    bindHandlers: function(){
-        var self = this;
-
-        // Handle select change
-
-        self.$filters.on('change', 'select', function(e){
-            e.preventDefault();
-
-            self.parseFilters();
-        });
-
-        // Handle reset click
-
-        self.$reset.on('click', function(e){
-            e.preventDefault();
-
-            self.$filters.find('select').val('');
-
-            self.parseFilters();
-        });
-    },
-
-    // The parseFilters method pulls the value of each active select option
-
-    parseFilters: function(){
-        var self = this;
-
-        // loop through each filter group and grap the value from each one.
-
-        for(var i = 0, group; group = self.groups[i]; i++){
-            group.active = group.$dropdown.val();
-        }
-
-        self.concatenate();
-    },
-
-    // The "concatenate" method will crawl through each group, concatenating filters as desired:
-
-    concatenate: function(){
-        var self = this;
-
-        self.outputString = ''; // Reset output string
-
-        for(var i = 0, group; group = self.groups[i]; i++){
-            self.outputString += group.active;
-        }
-
-        // If the output string is empty, show all rather than none:
-
-        !self.outputString.length && (self.outputString = 'all'); 
-
-        //console.log(self.outputString); 
-
-        // ^ we can check the console here to take a look at the filter string that is produced
-
-        // Send the output string to MixItUp via the 'filter' method:
-
-            if(self.$container.mixItUp('isLoaded')){
-                self.$container.mixItUp('filter', self.outputString);
-            }
-        }
-    };
-
-    // On document ready, initialise our code.
-
-    $(function(){
+    var checkboxFilter = {
       
-        // Initialize dropdownFilter code
+      // Declare any variables we will need as properties of the object
+      
+      $filters: null,
+      $reset: null,
+      groups: [],
+      outputArray: [],
+      outputString: '',
+      
+      // The "init" method will run on document ready and cache any jQuery objects we will need.
+      
+      init: function(){
+        var self = this; // As a best practice, in each method we will asign "this" to the variable "self" so that it remains scope-agnostic. We will use it to refer to the parent "checkboxFilter" object so that we can share methods and properties between all parts of the object.
+        
+        self.$filters = $('.event-filters');
+        self.$container = $('.event-tiles .grid');
+        
+        self.$filters.find('fieldset').each(function(){
+          self.groups.push({
+            $inputs: $(this).find('input'),
+            active: [],
+                tracker: false
+          });
+        });
+        
+        self.bindHandlers();
+      },
+      
+      // The "bindHandlers" method will listen for whenever a form value changes. 
+      
+      bindHandlers: function(){
+        var self = this;
+        
+        self.$filters.on('change', function(){
+          self.parseFilters();
+        });
+    
+      },
+      
+      // The parseFilters method checks which filters are active in each group:
+      
+      parseFilters: function(){
+        var self = this;
+     
+        // loop through each filter group and add active filters to arrays
+        
+        for(var i = 0, group; group = self.groups[i]; i++){
+          group.active = []; // reset arrays
+          group.$inputs.each(function(){ 
+            $(this).is(':checked') && group.active.push(this.value);
+          });
+            group.active.length && (group.tracker = 0);
+        }
+        
+        self.concatenate();
+      },
+      
+      // The "concatenate" method will crawl through each group, concatenating filters as desired:
+      
+      concatenate: function(){
+        var self = this,
+              cache = '',
+              crawled = false,
+              checkTrackers = function(){
+            var done = 0;
+            
+            for(var i = 0, group; group = self.groups[i]; i++){
+              (group.tracker === false) && done++;
+            }
 
-        dropdownFilter.init();
+            return (done < self.groups.length);
+          },
+          crawl = function(){
+            for(var i = 0, group; group = self.groups[i]; i++){
+              group.active[group.tracker] && (cache += group.active[group.tracker]);
 
-        // Instantiate MixItUp
+              if(i === self.groups.length - 1){
+                self.outputArray.push(cache);
+                cache = '';
+                updateTrackers();
+              }
+            }
+          },
+          updateTrackers = function(){
+            for(var i = self.groups.length - 1; i > -1; i--){
+              var group = self.groups[i];
 
-        $('.event-grid .grid').mixItUp({
+              if(group.active[group.tracker + 1]){
+                group.tracker++; 
+                break;
+              } else if(i > 0){
+                group.tracker && (group.tracker = 0);
+              } else {
+                crawled = true;
+              }
+            }
+          };
+        
+        self.outputArray = []; // reset output array
+
+          do{
+              crawl();
+          }
+          while(!crawled && checkTrackers());
+
+        self.outputString = self.outputArray.join();
+        
+        // If the output string is empty, show all rather than none:
+        
+        !self.outputString.length && (self.outputString = 'all'); 
+        
+        //console.log(self.outputString); 
+        
+        // ^ we can check the console here to take a look at the filter string that is produced
+        
+        // Send the output string to MixItUp via the 'filter' method:
+        
+          if(self.$container.mixItUp('isLoaded')){
+            self.$container.mixItUp('filter', self.outputString);
+          }
+      }
+    };
+      
+    // Initialize buttonFilter code
+          
+      checkboxFilter.init();
+          
+      // Instantiate MixItUp
+          
+      $('.event-tiles .grid').mixItUp({
             animation: {
                 effects: 'fade scale rotateX(90deg)',
                 reverseOut: true,
                 duration: 600
             },
             controls: {
-                enable: true // we won't be needing these
+                //enable: false
             },
             selectors: {
                 target: '.tile'
             },
             callbacks: {
                 onMixFail: function(){
-                    console.log('No items were found matching the selected filters.');
+                    $('.error-message').text('No events were found matching the selected filters.');
+                },
+                onMixStart: function() {
+                    $('.error-message').text('');
                 }
             },
             load: {
                 sort: 'date:desc'
             }
-        });   
-    });
+        });
+
+    // Toggle the event filters.
+    
+    function filterInit() {
+        /*if ( Modernizr.mq('only screen and (max-width: 767px)') ) {
+            
+        }*/
+
+        $('.event-grid #filters').slideToggle('close');
+            $('.event-grid #show-filters').on('click', function() {
+                $('.event-grid #filters').slideToggle();
+            });
+    }
+
+    filterInit();
 
 });
